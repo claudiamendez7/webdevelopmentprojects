16b3c028f6249ff252c120cd32eed02e0a8ab9eb
ClauMndz <claudiamendezt19@gmail.com>
2023-08-29 17:17:10 -0500

    icons

--- countriesApp/src/components/Navbar/Navbar.jsx
+++ countriesApp/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
 
