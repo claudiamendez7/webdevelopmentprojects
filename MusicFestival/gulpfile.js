516e469510ebb9aaf5935ba53581018c7f93b49d
ClauMndz <claudiamendezt19@gmail.com>
2023-10-31 20:25:23 -0500

    third commit

--- MusicFestival/gulpfile.js
+++ MusicFestival/gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(cb) {
   src('src/scss/**/*.scss') //Identify Sass file
+    .pipe(plumber())
     .pipe( sass()) //Compile
     .pipe( dest('build/css')) // Storage
-
-    cb(); //callback
+  cb(); //callback
 }
+
 function dev(cb) {
-    watch('src/scss/**/*.scss', css)
+    watch('src/scss/**/*.scss', css);
     cb();
 }
 
